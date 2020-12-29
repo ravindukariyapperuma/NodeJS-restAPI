1c638d22a22bca999c35d09a14330168f2f8f762
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 20:51:44 +0530

    save data using async await

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -10,20 +10,29 @@ router.get('/', (req, res, next) => {
     // res.send('getting a list of all products...');
 });
 
-router.post('/', (req, res, next) => {
-    console.log(req.body);
-    const product = new Product({
-        name: req.body.name,
-        price: req.body.price
-    });
-    product.save()
-    .then(result => {
-        console.log(result);
+router.post('/', async (req, res, next) => {
+    
+    try {
+        const product = new Product(req.body);
+        const result = await product.save();
         res.send(result);
-    })
-    .catch(err => {
-        console.log(err.message);
-    });
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    // const product = new Product({
+    //     name: req.body.name,
+    //     price: req.body.price
+    // });
+    // product.save()
+    // .then(result => {
+    //     console.log(result);
+    //     res.send(result);
+    // })
+    // .catch(err => {
+    //     console.log(err.message);
+    // });
+
 });
 
 router.get('/:id', (req, res, next) => {
