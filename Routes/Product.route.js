1362b1c075dfa66ffaa706679df77f0b0b3f6520
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 23:00:54 +0530

    delete product

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -7,9 +7,9 @@ const Product = require('../Models/Product.model');
 
 router.get('/', async (req, res, next) => {
     try {
-        // const results = await Product.find({}, {__v: 0});
+        const results = await Product.find({}, {__v: 0});
         // const results = await Product.find({}, { name: 1, price: 1, _id: 0});
-        const results = await Product.find({ price: 699 }, {});
+        // const results = await Product.find({ price: 699 }, {});
         res.send(results);
     } catch (error) {
         console.log(error.message);
@@ -56,8 +56,14 @@ router.patch('/:id', (req, res, next) => {
     res.send('updating a single product');
 });
 
-router.delete('/:id', (req, res, next) => {
-    res.send('deleting a single product');
+router.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        const result = await Product.findByIdAndDelete(id);
+        res.send(result);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 module.exports = router;
