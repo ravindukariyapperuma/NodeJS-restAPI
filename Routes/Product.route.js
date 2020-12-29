1e49f895bc912e9c042aac3baad521e5b5a2447f
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 16:12:28 +0530

    express router post

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -7,4 +7,8 @@ router.get('/', (req, res, next) => {
     res.send('getting a list of all products...');
 });
 
+router.post('/', (req, res, next) => {
+    res.send('product created');
+});
+
 module.exports = router;
