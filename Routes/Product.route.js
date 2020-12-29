969aa8ddd456ceb6d0af1b02892bff911d9b58d6
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 22:38:01 +0530

    get data

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -3,11 +3,17 @@ const { request } = require("express");
 const express = require('express');
 const router = express.Router();
 
-const Product = require('../Models/Product.model')
+const Product = require('../Models/Product.model');
 
-router.get('/', (req, res, next) => {
-    next(new Error("cannot get a list of all products"));
-    // res.send('getting a list of all products...');
+router.get('/', async (req, res, next) => {
+    try {
+        // const results = await Product.find({}, {__v: 0});
+        // const results = await Product.find({}, { name: 1, price: 1, _id: 0});
+        const results = await Product.find({ price: 699 }, {});
+        res.send(results);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 router.post('/', async (req, res, next) => {
