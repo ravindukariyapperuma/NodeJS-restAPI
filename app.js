f1c4909a98e15f3be5e167b9562e672c502620c3
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 15:28:39 +0530

    get method

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res, next) => {
+    console.log(req.url);
+    console.log(req.method);
+    res.send("i am the home route");
+});
+
 app.listen(3000, () => {
     console.log('Server started on port 3000...');
 });
