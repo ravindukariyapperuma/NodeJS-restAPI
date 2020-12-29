e5c63be62c179a6d1cdd277072704fa4a0ae8023
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-12-29 17:58:00 +0530

    mongodb connected

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/Products', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+})
+.then(() => {
+    console.log('Mongodb connected....');
+});
 
 const ProductRoute = require('./Routes/Product.route');
 app.use('/products', ProductRoute);
